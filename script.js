565439e331b1695cb93fa8ca2bdbc4e954c4779a
WillyW4sDeleted <145406289+WillyW4sDeleted@users.noreply.github.com>
2025-04-09 12:09:01 -0300

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+document .addEventListener ('DOMContentLoaded', function () {
+    let tamanhaAtualFonte = 1; 
+    
+    const aumentafontebotao = document.getElementById('aumentar-fonte');
+
+    aumentafontebotao.addEventListener('click', function 
+        
+    )
+}
+
+)
