e700af1c0b8a93a1b33731ddfc7e5489d64b709a
WillyW4sDeleted <145406289+WillyW4sDeleted@users.noreply.github.com>
2025-04-16 11:43:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-document.addEventListener ('DOMContentLoaded', function () {
-    let tamanhoAtualFonte = 1; 
-    
-    const aumentafontebotao = document.getElementById('aumentar-fonte');
+document.addEventListener('DOMContentLoaded', function () {
+    let tamanhoAtualFonte = 1;
 
-    aumentafontebotao.addEventListener('click', function () {
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+
+    aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
-    const diminuirfontebotao = document. getElementById ('diminuir-fonte'); 
-    
-}
 
-)
+    const  diminuirFonteBotao = document.getElementById ('diminuir-fonte');
+
+    diminuirFonteBotao.addEventListener ('click', function (){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+    
+    const alternaContraste = document.getElementById('alterna-contraste')
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+})
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
